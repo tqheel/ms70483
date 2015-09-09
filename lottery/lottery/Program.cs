74469fb0f00527b57c6c6c928ef4424f3035a317
Todd Qualls <tqualls@gmail.com>
2015-09-08 20:28:12 -0400

    Creates lottery console app

---
+++ lottery/lottery/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var max = 49;
+
+            var range = new int[max];
+
+            var random = new Random();
+
+            for(var i=0;i< max; i++)
+            {
+                range[i] = i+1;
+            }
+            
+            for(var x = 0; x < 6; x++)
+            {
+                var pickIndex = range[random.Next(max)];
+                var pick = range[pickIndex];
+                Console.WriteLine("Pick {0}: {1}", x+1, pick);
+            }
+        }
+    }
+}
